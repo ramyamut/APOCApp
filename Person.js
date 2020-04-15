af9f503c477e519c0c72ee20a46d1389ba5b60b6
ramyamut <51800150+ramyamut@users.noreply.github.com>
2020-04-15 19:48:22 -0400

    added database formats

---
+++ Person.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+// the host:port must match the location where you are running MongoDB
+// the "myDatabase" part can be anything you like
+mongoose.connect('mongodb+srv://admin:APOC123@clusterAPOC-kktca.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
+
+var Schema = mongoose.Schema;
+
+var personSchema = new Schema({
+firstname: {type: String, required: true},
+lastname: {type: String, required: true},
+phonenumber: String,
+region: String
+});
+
+// export personSchema as a class called Person
+module.exports = mongoose.model('Person', personSchema);
